f66c47e4909bbe8d7784b9481cc1fa8e0e481fba
Ataídes Cota <junior.cota.saqua@gmail.com>
2026-04-18 10:29:08 -0300

    alterações feitas pois nao abria no exp go

--- easyvacc/src/components/ui/collapsible.tsx
+++ easyvacc/src/components/ui/collapsible.tsx
@@ -1,4 +1,4 @@
-import { SymbolView } from 'expo-symbols';
+import { SymbolView, type SymbolViewProps } from 'expo-symbols';
 import { PropsWithChildren, useState } from 'react';
 import { Pressable, StyleSheet } from 'react-native';
 import Animated, { FadeIn } from 'react-native-reanimated';
@@ -19,7 +19,13 @@ export function Collapsible({ children, title }: PropsWithChildren & { title: st
         onPress={() => setIsOpen((value) => !value)}>
         <ThemedView type="backgroundElement" style={styles.button}>
           <SymbolView
-            name={{ ios: 'chevron.right', android: 'chevron_right', web: 'chevron_right' }}
+            name={
+              {
+                ios: 'chevron.right',
+                android: 'chevron_right',
+                web: 'chevron_right',
+              } as unknown as SymbolViewProps['name']
+            }
             size={14}
             weight="bold"
             tintColor={theme.text}
